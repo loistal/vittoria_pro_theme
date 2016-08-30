7d04dba9044dfc8fcf1c58c143250a025470c1dc
Lois Talagrand <lois.talagrand@gmail.com>
2016-08-29 17:04:39 -0930

    Correct initial commit

---
+++ __openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Vittoria Pro Theme',
+    'description': 'Ce module offre un thème professionel par défault, et contient des snippets supplémentaires pour permettre une personalisation rapide.',
+    'summary': 'Ce module offre un thème professionel et des snippets supplémentaires',
+    'category': 'Theme',
+    'version': '1.0',
+    'website': 'http://www.vittoria.pf',
+    'author': 'Lois Talagrand',
+    'depends': ['website'],
+    'data': [
+        'static/src/views/theme.xml',
+    ],
+    'images':[
+            'static/description/vittoria.png',
+    ],
+    'application': True,
+}
