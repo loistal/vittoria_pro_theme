55a9ea52f98e8de538e4ecb92ec95a42caeb3ce2
Lois Talagrand <lois.talagrand@gmail.com>
2016-09-05 14:01:29 -0930

    Monday version UNTESTED

--- __openerp__.py
+++ __openerp__.py
@@ -1,19 +1,22 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Vittoria Pro Theme',
-    'description': 'Ce module offre un thème professionel par défault, et contient des snippets supplémentaires pour permettre une personalisation rapide.',
-    'summary': 'Ce module offre un thème professionel et des snippets supplémentaires',
-    'category': 'Theme',
-    'version': '1.0',
-    'website': 'http://www.vittoria.pf',
-    'author': 'Lois Talagrand',
-    'depends': ['website'],
-    'data': [
-        'static/src/views/theme.xml',
-        'static/src/views/our_work.xml',
-    ],
-    'images':[
-            'static/description/vittoria.png',
-    ],
-    'application': True,
+'name': 'Vittoria Pro Theme',
+'description': 'Ce module offre un thème professionel par défault, et contient des snippets supplémentaires pour permettre une personalisation rapide.',
+'summary': 'Ce module offre un thème professionel et des snippets supplémentaires',
+'category': 'Theme',
+'version': '1.0',
+'website': 'http://www.vittoria.pf',
+'author': 'Lois Talagrand',
+'depends': ['website'],
+'data': [
+'static/src/views/theme.xml',
+'static/src/views/realisations.xml',
+'static/src/views/contact.xml',
+'static/src/views/custom_header_footer.xml',
+
+],
+'images':[
+'static/description/vittoria.png',
+],
+'application': True,
 }
